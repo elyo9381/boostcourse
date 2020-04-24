35842545f5f0ce5cd7207fbf48d2f4a22f5dee0b
elyo9381 <won4413@daum.net>
2020-04-24 23:12:27 +0900

    tab ui test & implemention

--- 3-1/test/test.js
+++ 3-1/test/test.js
@@ -1,10 +1,25 @@
-var target = document.querySelector("div");
+function makeTemplate(data,clickedName) {
+    let html = document.getElementById("tabcontent").innerHTML;
+    let resultHTML = "";
+    for(let i = 0; i<data.length; i++) {
+        resultHTML = html.replace("{name}",data[i].name)
+                         .replace("{favorites}",data[i].favorites.join(" "));
+    }
+}
 
-target.addEventListener("mouseover",function() {
-    var preleft = parseInt(target.style.left);
-    var pretop = parseInt(target.style.top);
+function sendAjax(url, clickedName) {
+    let oReq = new XMLHttpRequest();
+    oReq.addEventListener("load",function(){
+        let data = JSON.parse(oReq.responseText);
+        makeTemplate(data,clickedName);
+    });
+    oReq.open("GET",url);
+    oReq.send();
+}
 
-    target.style.top = pretop + 101 + "vw";
-    target.style.left = preleft + 101 + "vw";
-
+let tabmenu = document.querySelector(".tabmenu");
+tabmenu.addEventListener("click",function(){
+    sendAjax("./json.txt",evt.target.innerText);
+    // console.log(evt.target); // target은 element의 관한 정보를 반환함
+    // console.log(evt.target.innerText); // element의 text를 반환함
 });
